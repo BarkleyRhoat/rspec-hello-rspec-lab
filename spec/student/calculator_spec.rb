4cf44f86bcc21aed192dccd8d96b04aed94ef9b2
BarkleyRhoat <barkleyrhoat@gmail.com>
2026-08-24 09:24:31 -0400

    hello rspec lab

--- spec/student/calculator_spec.rb
+++ spec/student/calculator_spec.rb
@@ -1,5 +1,22 @@
 # frozen_string_literal: true
 
 describe Calculator do
-  # ...student specs...
+  it "adds two numbers" do 
+    expect(Calculator.new.add(2, 3)).to eq(5)
+  end
+
+  it "subtracts two numbers" do
+    expect(Calculator.new.subtract(5, 1)).to eq(4)
+  end
+
+  it "multiplies two numbers" do
+    expect(Calculator.new.multiply(2, 2)).to eq(4)
+  end
+
+  it "divides two numbers" do 
+    expect(Calculator.new.divide(10, 5)).to eq(2)
+  end
+  it "returns an error or special value when dividing by zero" do 
+    expect { Calculator.new.divide(5, 0) }.to raise_error(ZeroDivisionError)
+  end
 end
